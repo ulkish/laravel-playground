64ad6b7de1e78bc95019caed0890443c63c32f2e
ulkish <dev.hugomoran@gmail.com>
2020-01-25 15:57:40 -0300

    Created accessor for Post titles.

--- app/Post.php
+++ app/Post.php
@@ -6,5 +6,25 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-    //
+    public function getTitleAttribute($value)
+    {
+
+        $ignore = ['a', 'and', 'at', 'but', 'for', 'in', 'the', 'to', 'with'];
+
+        $title = explode(' ', $value);
+
+        $modifiedTitle = [];
+
+        foreach ($title as $word)
+        {
+            if (! in_array(strtolower($word), $ignore))
+            {
+                $modifiedTitle[] = ucfirst($word);
+            } else {
+                $modifiedTitle[] = strtolower($word);
+            }
+        }
+
+        return join(' ', $modifiedTitle);
+    }
 }
